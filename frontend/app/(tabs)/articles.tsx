d858ba99c8dd8d1cb73e00109397e513e1122774
Kostek <ksytnik73kwer@gmail.com>
2024-06-29 15:59:03 +0200

    Remove explore page, add basic articles + quiz pages

---
+++ frontend/app/(tabs)/articles.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Articles () {
+    return (
+     <View>
+        <Text>Articles</Text>
+     </View>   
+    )
+}
---
+++ frontend/app/(tabs)/quiz.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Articles () {
+    return (
+     <View>
+        <Text>Quizzes will be here</Text>
+     </View>   
+    )
+}
