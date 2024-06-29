a1f282feb08c1a02bce1f0f4e55e28237393e071
Kostek <ksytnik73kwer@gmail.com>
2024-06-29 21:04:51 +0200

    Install and configure TailwindWind for using TailwindCSS with Expo app

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,13 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: ["nativewind/babel"],
+}
+
