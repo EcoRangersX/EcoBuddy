8d4a09344bdf758ac6e83da9f2fdbf151b89c25f
Kostek <ksytnik73kwer@gmail.com>
2024-07-05 09:21:45 +0200

    Remove CodeQl from github workflows and update main page

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,13 +1,20 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
+import { TextInput } from 'react-native-paper';
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text>Main page</Text>
+      <Text className="flex justify-center bg-green-700 text-[23px]">
+        Main age
+      </Text>
+      <View className="p-5">
+        <TextInput
+        mode="outlined"
+        label="Outlined input"
+        placeholder="Type something"
+        right={<TextInput.Affix text="/100" />}
+      />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  
-});
