ecc1ce91085dd4bcac4ae93e202a2b5a77c21439
Kostek <ksytnik73kwer@gmail.com>
2024-07-07 12:27:02 +0200

    chore: Update AiAssistantScreen and add QuestionInput component

--- frontend/app/(tabs)/ai-assistant.tsx
+++ frontend/app/(tabs)/ai-assistant.tsx
@@ -1,11 +1,25 @@
-import { View, Text } from "react-native";
-import Header from "@/components/Header";
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Header from '@/components/Header';
+import QuestionInput from '@/components/QuestionText';
 
 export default function AiAssistantScreen() {
   return (
-    <View>
+    <View style={styles.container}>
       <Header />
-      <Text>AiAssistant</Text>
+      <View style={styles.inputContainer}>
+        <QuestionInput />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+});
