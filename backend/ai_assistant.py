4ffd2c39afa9687130243953888bc9c514ee9754
Kostek <ksytnik73kwer@gmail.com>
2024-07-09 19:46:08 +0200

    chore: Refactor Assistant class and improve code readability

--- backend/ai_assistant.py
+++ backend/ai_assistant.py
@@ -2,15 +2,15 @@ from groq import Groq
 
 
 class Assistant:
-    def __init__(self, aiprovider: Groq):
+    def __init__(self, ai_provider: Groq):
         self.PROMPT = "Check if this question is about ecology.\
-                       If so go straight to the answer otherwise just say 'I can't answer it' "
+                       If so, go straight to the answer otherwise just say 'I can't answer it' "
 
-        self.modelprovider = aiprovider
+        self.model_provider = ai_provider
 
     # dzwonienie do api groq o odpowiedź na pytanie użytkownika
-    def get_ai_response(self, prompt):
-        response = self.modelprovider.chat.completions.create(  # zapytanie do api
+    def get_ai_response(self, prompt: str):
+        response = self.model_provider.chat.completions.create(  # zapytanie do api
             model="llama3-70b-8192",  # deklaracja modelu językowego
             messages=[{"role": "user", "content": prompt}],
             temperature=1,
@@ -32,8 +32,8 @@ class Assistant:
         return constructed_response
 
     # post request
-    def request_post(self, prompt):
-        prompt = prompt  # + self.PROMPT # tworzenie odpowiedniego prompta  do naszych zapotrzebowań
-        response = self.get_ai_response(prompt=prompt)
+    def request_post(self, user_prompt: str) -> dict:
+        # prompt = prompt  # + self.PROMPT # tworzenie odpowiedniego prompta  do naszych zapotrzebowań
+        response = self.get_ai_response(prompt=user_prompt)
 
-        return {"Response": response}  # zwrot jsona jako odpowiedź na post request
+        return {"response": response}  # zwrot jsona jako odpowiedź na post request
