6c663afbfaf3b7dbb2eeb9f74a19e19fd918e325
Kostek <ksytnik73kwer@gmail.com>
2024-07-26 17:24:20 +0300

    chore: Update HomeScreen component with AQIComponent and remove unused code

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,19 +1,12 @@
 import { View, Text } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import AQIComponent from '@/components/AQIComponent';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text className="flex justify-center bg-green-700 text-[23px]">
-        Main age
-      </Text>
-      <View className="p-5">
-        <TextInput
-        mode="outlined"
-        label="Outlined input"
-        placeholder="Type something"
-        right={<TextInput.Affix text="/100" />}
-      />
+    <View className="flex-1 items-center justify-center">
+      <Text>Home Screen</Text>
+      <View className="">
+        <AQIComponent />
       </View>
     </View>
   );
