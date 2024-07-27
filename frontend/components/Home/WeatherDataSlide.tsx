56fa14eeb9de410724fbbc546cea0215ac4e9911
Kostek <ksytnik73kwer@gmail.com>
2024-07-27 23:06:59 +0300

    Add all other Weather Elements and allow Horizontal Scrolling them

--- frontend/components/Home/WeatherDataSlide.tsx
+++ frontend/components/Home/WeatherDataSlide.tsx
@@ -1,12 +1,15 @@
 import WeatherDataElement from '@/components/Home/WeatherDataElement';
-import { View } from 'react-native';
+import { ScrollView, ViewStyle } from 'react-native';
 
 export default function WeatherDataSlide() {
   return (
-    <View className="flex flex-row p-5 items-center">
-      <WeatherDataElement elementName="wind" value={26} unit="km/h" status="low" />
-      <WeatherDataElement elementName="pollen" value={26} unit="%" status="low" />
-      <WeatherDataElement elementName="pollen" value={26} unit="%" status="low" />
-    </View>
+    <ScrollView horizontal={true} contentContainerStyle={{ flexDirection: 'row', padding: 20, gap: 20, alignItems: 'center' } as ViewStyle}>
+      <WeatherDataElement elementName="temp." value={26} unit="°C" status="low" />
+      <WeatherDataElement elementName="wind" value={26} unit="km/h" status="moderate" />
+      <WeatherDataElement elementName="pollen" value={26} unit="" status="low" />
+      <WeatherDataElement elementName="uv index" value={7} unit="" status="high" />
+      <WeatherDataElement elementName="humidity" value={26} unit="%" status="low" />
+      <WeatherDataElement elementName="pressure" value={26} unit="hPa" status="high" />
+    </ScrollView>
   );
 }
