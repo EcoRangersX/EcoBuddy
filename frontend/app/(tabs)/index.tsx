d3903f14215ce044572d96e5f959ee2b682b8479
Kostek Sytnyk <ksytnik73kwer@gmail.com>
2024-07-27 12:52:42 +0300

    Create AQI component (#7)
    * Add react-native-circular-progress dependency
    
    * feat: Add AQIComponent for displaying air quality index
    
    * chore: Update HomeScreen component with AQIComponent and remove unused code
    
    * chore: Update tab layout component with Expo Router
    
    * Update initial route name in TabLayout component
    
    * Update _layout.tsx with Expo Router and initial route settings
    
    * chore: Update AQIComponent with new styling and circular progress bar
    
    * chore: Update HomeScreen component with Header and AQIComponent
    
    * feat: Add LocationIcon component and update AQIComponent styling
    
    * chore: Add ViewDetailsIcon component and update AQIComponent with new icon
    
    * feat: Update AQIComponent with ViewDetailsIcon and new styling

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,19 +1,13 @@
-import { View, Text } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import { View } from 'react-native';
+import AQIComponent from '@/components/AQIComponent';
+import Header from '@/components/Header';
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text className="flex justify-center bg-green-700 text-[23px]">
-        Main age
-      </Text>
-      <View className="p-5">
-        <TextInput
-        mode="outlined"
-        label="Outlined input"
-        placeholder="Type something"
-        right={<TextInput.Affix text="/100" />}
-      />
+      <Header />
+      <View className="mt-10 items-center">
+        <AQIComponent />
       </View>
     </View>
   );
---
+++ frontend/components/AQIComponent.tsx
@@ -0,0 +1,48 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+import Svg from 'react-native-svg';
+import { AnimatedCircularProgress  } from 'react-native-circular-progress';
+import LocationIcon from '@/components/icons/LocationIcon';
+import ViewDetailsIcon from '@/components/icons/ViewDetailsIcon';
+
+const AQIComponent = () => {
+  const aqiValue = 36; // Static data for now
+  const aqiStatus = "Good"; // Static data for now
+  const location = "Gniezno, Greater Poland"; // Static data for now
+
+  return (
+    <View className="bg-[#38d26e] p-5 rounded-[30px] w-[80%] items-center justify-center">
+      <View className='flex flex-row items-center mb-3'>
+        <LocationIcon color='white' />
+        <Text className="text-white text-base ml-1">{location}</Text>
+      </View>
+      <View className="items-center justify-center">
+        <Svg width={170} height={170} viewBox="0 0 170 170">
+          <AnimatedCircularProgress
+            size={170}
+            width={8}
+            fill={aqiValue}
+            tintColor="#3dbcab"
+            backgroundColor="#fff"
+            lineCap="round"
+          >
+            {
+              () => (
+                <View className='flex flex-col'>
+                  <Text className="text-white text-4xl self-center">{aqiValue}</Text>
+                  <Text className="text-white text-lg self-center">{aqiStatus}</Text>
+                </View>
+              )
+            }
+          </AnimatedCircularProgress>
+        </Svg>
+      </View>
+      <Text className="text-white text-sm mb-4">EU AQI</Text>
+      <TouchableOpacity className="flex flex-row items-center bg-white px-7 py-2 rounded-full">
+        <Text className="text-black text-base mr-2">View Details</Text>
+        <ViewDetailsIcon size={30} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default AQIComponent;
