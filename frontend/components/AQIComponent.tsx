5f4d91ced0fd7098b55469e2dcb7d08d21b6f82a
Kostek <ksytnik73kwer@gmail.com>
2024-07-27 10:56:25 +0300

    chore: Update AQIComponent with new styling and circular progress bar

--- frontend/components/AQIComponent.tsx
+++ frontend/components/AQIComponent.tsx
@@ -1,5 +1,5 @@
 import { View, Text, TouchableOpacity } from 'react-native';
-import Svg, { Circle } from 'react-native-svg';
+import Svg from 'react-native-svg';
 import { AnimatedCircularProgress  } from 'react-native-circular-progress';
 
 const AQIComponent = () => {
@@ -8,38 +8,32 @@ const AQIComponent = () => {
   const location = "Gniezno, Greater Poland"; // Static data for now
 
   return (
-    <View className="bg-[#3ac5c9] p-5 rounded-3xl items-center justify-center">
-      <Text className="text-white text-base mb-2">{location}</Text>
-      <View className="items-center justify-center mb-2">
-        <Svg width={150} height={150} viewBox="0 0 150 150">
-          <Circle
-            cx="75"
-            cy="75"
-            r="70"
-            stroke="#3dbf9f"
-            strokeWidth="5"
-            fill="none"
-          />
+    <View className="bg-[#38d26e] p-5 rounded-[30px] w-[80%] items-center justify-center">
+      <Text className="text-white text-base mb-3">{location}</Text>
+      <View className="items-center justify-center">
+        <Svg width={170} height={170} viewBox="0 0 170 170">
           <AnimatedCircularProgress
-            size={150}
-            width={5}
+            size={170}
+            width={8}
             fill={aqiValue}
-            tintColor="#00e0ff"
-            backgroundColor="#3d5875"
+            tintColor="#3dbcab"
+            backgroundColor="#fff"
             lineCap="round"
           >
             {
               () => (
-                <Text className="text-white text-4xl absolute self-center mt-12">{aqiValue}</Text>
+                <View className='flex flex-col'>
+                  <Text className="text-white text-4xl self-center">{aqiValue}</Text>
+                  <Text className="text-white text-lg self-center">{aqiStatus}</Text>
+                </View>
               )
             }
           </AnimatedCircularProgress>
         </Svg>
       </View>
-      <Text className="text-white text-lg mb-1">{aqiStatus}</Text>
       <Text className="text-white text-sm mb-4">EU AQI</Text>
       <TouchableOpacity className="bg-white px-5 py-2 rounded-full">
-        <Text className="text-[#3ac5c9] text-base">view details</Text>
+        <Text className="text-[#3ac5c9] text-base">View Details</Text>
       </TouchableOpacity>
     </View>
   );
