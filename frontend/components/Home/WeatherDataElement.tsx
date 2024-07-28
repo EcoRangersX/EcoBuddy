108bbf3a0480f9888f11f0184a2e9b8b5497ae03
Kostek <ksytnik73kwer@gmail.com>
2024-07-28 10:48:23 +0300

    Add windLevel, tempLevel, status functions to display Weather Data appropriately

--- frontend/components/Home/WeatherDataElement.tsx
+++ frontend/components/Home/WeatherDataElement.tsx
@@ -12,14 +12,12 @@ import capitalize from '@/utility/capitalizeWord';
 interface WeatherDataElementProps {
   elementName: string;
   value: number;
-  status: string;
   unit: string;
 }
 
 export default function WeatherDataElement({
   elementName,
   value,
-  status,
   unit,
 }: WeatherDataElementProps) {
   const elementIcon = (elementName: string) => {
@@ -34,32 +32,66 @@ export default function WeatherDataElement({
         return <WindIcon />;
       case 'pressure':
         return <PressureIcon />;
-      case "uv index":
+      case 'uv index':
         return <UvIndexIcon />;
       default:
         return <TemperatureIcon />;
     }
   };
 
-  const borderColor = ( status: string ) => {
-    switch (status) {
-      case 'low':
-        return 'border-[#03c13d]';
-      case 'moderate':
-        return 'border-[#f6b930]';
-      case 'high':
-        return 'border-[#ff0000]';
-      default:
-        return 'border-[#03c13d]';
+  const borderColor = (status: string) => {
+    if (elementName === "temp.") {
+      switch (status) {
+        case 'cold':
+          return 'border-[#03c13d]';
+        case 'cool':
+          return 'border-[#56cade]';
+        case 'moderate':
+          return 'border-[#f6b930]';
+        case 'warm':
+          return 'border-[#ff7f00]';
+        case 'hot':
+          return 'border-[#ff0000]';
+        default:
+          return 'border-[#03c13d]';
+      }
     }
+    return 'border-[#2abb49]';
+  };
+
+  const tempLevel = () => {
+    if (value < 0) return 'cold';
+    if (value >= 0 && value < 15) return 'cool';
+    if (value >= 15 && value < 25) return 'moderate';
+    if (value >= 25 && value < 35) return 'warm';
+    if (value >= 35) return 'hot';
+    return 'warm';
+  };
+
+  const windLevel = () => {
+    if (value < 10) return 'low';
+    if (value >= 10 && value < 20) return 'moderate';
+    if (value >= 20 && value < 30) return 'high';
+    if (value >= 30) return 'very high';
+    return 'moderate';
   }
+
+  const status = (elementName: string) => {
+    if (elementName === 'temp.') {
+      return tempLevel();
+    } else if (elementName === 'wind') {
+      return windLevel();
+    }
+    return "normal";
+  };
 
   const Styles = {
     containerStyles:
-      'flex bg-[#56cade] p-3 rounded-2xl items-center shadow-2xl shadow-[#5662f6] relative',
+      'flex bg-[#199ee7] p-3 rounded-2xl items-center shadow-2xl shadow-[#5662f6] relative',
     rowStyles: 'flex flex-row items-center gap-7 relative',
-    elementTextStyles:
-      `flex text-white text-base border-b-[4px] ${borderColor(status)} pb-[px]`,
+    elementTextStyles: `flex text-white text-base border-b-[4px] ${borderColor(
+      status(elementName),
+    )} pb-[px]`,
     flexColStyles: 'flex flex-col items-center mr-10',
     flexRowStyles: 'flex flex-row items-baseline',
     valueTextStyles: 'text-white text-3xl',
@@ -80,7 +112,7 @@ export default function WeatherDataElement({
           <Text className={Styles.valueTextStyles}>{value}</Text>
           <Text className={Styles.unitTextStyles}>{unit}</Text>
         </View>
-        <Text className={Styles.statusTextStyles}>{status.toUpperCase()}</Text>
+        <Text className={Styles.statusTextStyles}>{status(elementName).toUpperCase()}</Text>
       </View>
     </View>
   );
