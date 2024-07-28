9b7b0b39276136db6f8118cf0cb2ca825324c9ad
Kostek <ksytnik73kwer@gmail.com>
2024-07-28 17:00:58 +0300

    Add ChemicalElementsSlider to HomeScreen and ScrollView to see the whole Screen content

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,18 +1,22 @@
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import AQIComponent from '@/components/Home/AQIComponent';
 import Header from '@/components/Header';
 import WeatherDataSlider from '@/components/Home/WeatherDataSlider';
+import ChemicalElementsSlider from '@/components/Home/ChemicalElementsSlider';
 
 export default function HomeScreen() {
   return (
-    <View>
+    <ScrollView>
       <Header />
-      <View className='mt-5 items-center'>
+      <View className="mt-5 items-center">
         <AQIComponent />
       </View>
-      <View className='mt-5 p-5'>
+      <View className="mt-5 p-5">
         <WeatherDataSlider />
       </View>
-    </View>
+      <View className="">
+        <ChemicalElementsSlider />
+      </View>
+    </ScrollView>
   );
 }
