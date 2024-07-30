f1e204ffdd057f0967852b4da26c1888095111ab
Kostek <ksytnik73kwer@gmail.com>
2024-07-30 15:25:47 +0300

    Add EducationSection component HomeScreen

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -3,6 +3,8 @@ import AQIComponent from '@/components/Home/AQIComponent';
 import Header from '@/components/Header';
 import WeatherDataSlider from '@/components/Home/WeatherDataSlider';
 import ChemicalElementsSlider from '@/components/Home/ChemicalElementsSlider';
+import EducationSection from '@/components/Home/EducationSection';
+import { aiQuestions, quizTitles } from '@/constants/EducationItems';
 
 export default function HomeScreen() {
   return (
@@ -14,8 +16,10 @@ export default function HomeScreen() {
       <View className="mt-5 p-5">
         <WeatherDataSlider />
       </View>
-      <View className="">
-        <ChemicalElementsSlider />
+      <ChemicalElementsSlider />
+      <View className="flex flex-col gap-5 mt-5 ">
+        <EducationSection title='Quiz' items={quizTitles} />
+        {/* <EducationSection title='Ai Questions' items={aiQuestions} /> */}
       </View>
     </ScrollView>
   );
