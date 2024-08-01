2c8150d629a0f154b7ab91d5f3e17e1ad7522e95
Kostek <ksytnik73kwer@gmail.com>
2024-08-01 15:21:02 +0300

    Update AQIComponent with useRouter hook and handleViewDetails function

--- frontend/components/Home/AQIComponent.tsx
+++ frontend/components/Home/AQIComponent.tsx
@@ -4,6 +4,7 @@ import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import { LocationIcon, ViewDetailsIcon } from '@/components/icons/HomeIcons';
 import capitalize from '@/utility/capitalizeWord';
 import { LinearGradient } from 'expo-linear-gradient';
+import { useRouter } from 'expo-router';
 
 interface AQIComponentProps {
   value: number;
@@ -12,13 +13,19 @@ interface AQIComponentProps {
 }
 
 const AQIComponent = ({ value, status, location }: AQIComponentProps) => {
+  const router = useRouter();
+
+  const handleViewDetails = () => {
+    router.push('/aqi-details');
+  };
+
   return (
     <LinearGradient
       colors={['rgba(30,158,105,1)', 'rgba(89,192,230,1)']}
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 1 }}
       className="rounded-[30px] w-[90%]">
-      <View className='p-5 items-center justify-center'>
+      <View className="p-5 items-center justify-center">
         <View className="flex flex-row items-center mb-3">
           <LocationIcon color="white" />
           <Text className="text-white text-base ml-1">{location}</Text>
@@ -46,7 +53,9 @@ const AQIComponent = ({ value, status, location }: AQIComponentProps) => {
           </Svg>
         </View>
         <Text className="text-white text-sm mb-4">EU AQI</Text>
-        <TouchableOpacity className="flex flex-row items-center bg-white px-7 py-2 rounded-full">
+        <TouchableOpacity
+          onPress={handleViewDetails}
+          className="flex flex-row items-center bg-white px-7 py-2 rounded-full">
           <Text className="text-black text-base mr-2">View Details</Text>
           <ViewDetailsIcon size={30} />
         </TouchableOpacity>
