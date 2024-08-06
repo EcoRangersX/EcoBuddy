c3fd14750d1c0a5046f0494509a1671f257ca705
Kostek <ksytnik73kwer@gmail.com>
2024-08-06 16:49:34 +0300

    Refactor AqiChemicalItem structure and styling

--- frontend/components/Aqi-Details/AqiChemicalItem.tsx
+++ frontend/components/Aqi-Details/AqiChemicalItem.tsx
@@ -7,11 +7,11 @@ interface AqiChemicalItemProps {
 
 const AqiChemicalItem = ({ name, description }: AqiChemicalItemProps) => {
     return (
-        <View className="flex-row items-center mb-4 bg-teal-100 p-4 rounded-lg shadow-sm">
-            <View className="flex-1 p-2 bg-teal-300 rounded-full mr-2">
+        <View className="flex flex-row items-center mb-5 p-2 bg-[#bff1f9] rounded-[30px] shadow-lg shadow-black">
+            <View className="p-2 bg-[#3baec2] rounded-full absolute top-0 left-5 w-1/3">
                 <Text className="text-white font-bold text-center">{name}</Text>
             </View>
-            <View className="flex-3 p-2 bg-white rounded-lg">
+            <View className="flex items-baseline mt-10">
                 <Text className="text-teal-800">{description}</Text>
             </View>
         </View>
