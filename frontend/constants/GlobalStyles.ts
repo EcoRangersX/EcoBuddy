5233cbb77a1e211c1e3af19c8a71c12d88e75d6f
Kostek <ksytnik73kwer@gmail.com>
2024-08-06 15:31:32 +0300

    feat: Add global styles for BoxStyles and sectionTitle components

--- frontend/constants/GlobalStyles.ts
+++ frontend/constants/GlobalStyles.ts
@@ -1,7 +1,20 @@
-const BoxStyles = {
-  borderColor: "#2cec53",
-  borderRadius: 30,
-  borderWidth: 2,
-};
+import { StyleSheet } from 'react-native';
 
-export { BoxStyles };
+const styles = StyleSheet.create({
+  BoxStyles: {
+    borderColor: '#2cec53',
+    borderRadius: 30,
+    borderWidth: 2,
+  },
+  sectionTitle: {
+    backgroundColor: '#62cabc',
+    borderRadius: 30,
+    paddingVertical: 8,
+    shadowColor: '#00',
+    shadowOffset: { width: 10, height: 10 },
+    shadowOpacity: 0.5,
+    shadowRadius: 60,
+  },
+});
+
+export { styles };
