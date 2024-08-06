532aa222f57e86ad5dc99d32178355813018f9aa
Kostek <ksytnik73kwer@gmail.com>
2024-08-06 15:32:19 +0300

    Refactor AqiMeasureExplanationSection and AqiMeasureInfoBox styling

--- frontend/components/Aqi-Details/AqiMeasureExplanationSection.tsx
+++ frontend/components/Aqi-Details/AqiMeasureExplanationSection.tsx
@@ -1,15 +1,20 @@
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import AqiMeasureInfoBox from './AqiMeasureInfoBox';
 import AqiChemicalList from './AqiChemicalList';
+import { styles } from '@/constants/GlobalStyles';
 
 const AqiMeasureExplanationSection = () => {
-    return (
-        <View className="p-6 bg-white rounded-lg shadow-xl">
-            <AqiMeasureInfoBox />
-            <AqiChemicalList />
-        </View>
-    );
+  return (
+    <View className=" rounded-lg shadow-xl">
+      <View style={styles.sectionTitle} className='p-2'>
+        <Text className="text-white font-bold text-base">
+          How is AQI Measured?
+        </Text>
+      </View>
+      <AqiMeasureInfoBox />
+      <AqiChemicalList />
+    </View>
+  );
 };
 
 export default AqiMeasureExplanationSection;
-
