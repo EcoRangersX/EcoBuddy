f1348da8e68a7ab65c9c5ee40b8fdb62fa9f5f9c
Kostek Sytnyk <ksytnik73kwer@gmail.com>
2024-08-08 10:48:43 +0300

    Implement Indoor Air Quality Tips Section (#30)
    * Implement AirQualityTip component and StaticData file to store the static info
    
    * Add AirQualityTipList component and update AQIDetails screen with it

---
+++ frontend/constants/StaticData.ts
@@ -0,0 +1,9 @@
+const airQualityTips = [
+  'Change Filters Regularly',
+  'Invest In An Air Purifier',
+  'Get Plants',
+  'Avoid Smoking Inside',
+  'Use Natural Cleaning Products',
+];
+
+export { airQualityTips };
