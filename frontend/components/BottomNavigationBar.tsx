39d6499fa964b617315a73ce257666d6173147b1
Kostek <ksytnik73kwer@gmail.com>
2024-08-08 17:28:26 +0300

    Implement a BottomNavigationBar component to reuse in further screens

---
+++ frontend/components/BottomNavigationBar.tsx
@@ -0,0 +1,72 @@
+import { Tabs } from 'expo-router';
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+
+const BottomNavigationBar = () => {
+  return (
+    <Tabs
+      initialRouteName="index"
+      screenOptions={({}) => ({
+        headerShown: false,
+      })}>
+      <Tabs.Screen
+        name="quizzes"
+        options={{
+          title: 'Quiz',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon
+              provider="MaterialIcons"
+              name="question-answer"
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="articles"
+        options={{
+          title: 'Articles',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon provider="MaterialIcons" name="article" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon provider="Ionicons" name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="ai-assistant"
+        options={{
+          title: 'Ai-Assistant',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon
+              provider="MaterialIcons"
+              name="assistant"
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon
+              provider="FontAwesome6"
+              name="circle-user"
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export { BottomNavigationBar };
