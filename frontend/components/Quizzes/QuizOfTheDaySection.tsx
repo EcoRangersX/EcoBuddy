516d944d0b71c89d4e1bcd168ce78c49bd3ff2ab
Kostek <ksytnik73kwer@gmail.com>
2024-08-11 10:49:51 +0200

    Add QuizOfTheDaySection component

---
+++ frontend/components/Quizzes/QuizOfTheDaySection.tsx
@@ -0,0 +1,30 @@
+import StartQuiz from './StartQuiz';
+import { View, Text } from 'react-native';
+import { StartQuizProps } from '@/types/quizzes';
+
+const QuizOfTheDaySection = ({
+  quiz_id,
+  level,
+  questionsCount,
+  bgColor = 'white',
+  titleBgColor = '#74c6d4',
+  title,
+  description,
+}: StartQuizProps) => {
+  return (
+    <View className="p-3">
+      <Text className="text-xl mb-4">Quiz of the day</Text>
+      <StartQuiz
+        quiz_id={quiz_id}
+        level={level}
+        questionsCount={questionsCount}
+        bgColor={bgColor}
+        titleBgColor={titleBgColor}
+        title={title}
+        description={description}
+      />
+    </View>
+  );
+};
+
+export default QuizOfTheDaySection;
