9bd8aa9e2114f79fa64d5116eba9f0a21d3f770b
Kostek <ksytnik73kwer@gmail.com>
2024-08-11 13:53:43 +0200

    Update YourScoresSection with QuizResult comp. and refactor Quizzes Screen

--- frontend/components/Quizzes/YourScoresSection.tsx
+++ frontend/components/Quizzes/YourScoresSection.tsx
@@ -1,5 +1,6 @@
 import { Text, View } from 'react-native';
 import CurrentQuizStreak from './CurrentQuizStreak';
+import QuizResult from './QuizResult';
 
 interface YourScoresSectionProps {
   streak: number;
@@ -9,9 +10,10 @@ const YourScoresSection = ({ streak }: YourScoresSectionProps) => {
   return (
     <View className="p-3">
       <Text className="text-xl mb-4">Your Scores</Text>
-      <View>
+      <View className=''>
         <CurrentQuizStreak streak={streak} />
       </View>
+      <QuizResult title='Climate Change' correctAnswers={12} quizQuestions={15} />
     </View>
   );
 };
