6b6de91bfce46f80795a097c0cf05022c5c7b982
Kostek <ksytnik73kwer@gmail.com>
2024-08-11 09:22:26 +0200

    Add optional params to customize StartQuiz component styling

--- frontend/components/Quizzes/StartQuiz.tsx
+++ frontend/components/Quizzes/StartQuiz.tsx
@@ -4,6 +4,8 @@ import { StartQuizIcon } from '../Icons/QuizzesIcons';
 type StartQuizProps = {
   level: string;
   questionsCount: number;
+  bgColor?: string;
+  titleBgColor?: string;
   title: string;
   description: string;
   onStart?: () => void;
@@ -12,11 +14,15 @@ type StartQuizProps = {
 const StartQuiz = ({
   level,
   questionsCount,
+  bgColor = 'white',
+  titleBgColor = '#74c6d4',
   title,
   description,
 }: StartQuizProps) => {
   return (
-    <View className="p-4 bg-white mb-4 rounded-[30px] shadow-md shadow-black">
+    <View
+      style={{ backgroundColor: bgColor }}
+      className="p-4 mb-4 rounded-[30px] shadow-md shadow-black">
       <View className="flex-row gap-2 mb-3">
         <Text className="bg-white text-black py-1 px-2 shadow-md shadow-black rounded-full text-xs">
           {level}
@@ -25,7 +31,11 @@ const StartQuiz = ({
           Questions: {questionsCount}
         </Text>
       </View>
-      <View className="flex-row items-center justify-between bg-[#74c6d4] px-4 py-2 rounded-full">
+      <View
+        style={{
+          backgroundColor: titleBgColor,
+        }}
+        className="flex-row items-center justify-between px-4 py-2 rounded-full">
         <Text className="text-white font-bold mr-2">{title}</Text>
         <StartQuizIcon quiz_id={16} />
       </View>
