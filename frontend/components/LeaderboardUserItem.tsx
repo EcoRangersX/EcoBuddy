17d472addc617b63f5d3956a8452076137836fa3
Kostek <ksytnik73kwer@gmail.com>
2024-08-11 18:02:18 +0200

    Update LeaderboardUserItem to use user_id and profile_image_url

--- frontend/components/LeaderboardUserItem.tsx
+++ frontend/components/LeaderboardUserItem.tsx
@@ -4,24 +4,25 @@ import { LeaderboardUserItemProps } from '@/types/quizzes';
 
 const LeaderboardUserItem = ({
   username,
-  username_profile_url,
+  user_id,
+  profile_image_url,
   score,
 }: LeaderboardUserItemProps) => {
-  const handleProfileImagePress = () => {
-    console.log('Profile Image Pressed');
+  const handleProfileImagePress = (user_id: number) => {
+    console.log(`Navigating to user profile with id: ${user_id}`);
   };
 
   return (
-    <View className="flex-row bg-white shadow-sm items-center  justify-between shadow-black rounded-full p-3 mb-3 border-2 border-[#4ac965]">
+    <View className="flex-row bg-white shadow-sm items-center justify-between shadow-black rounded-full p-3 mb-4 border-2 border-[#4ac965]">
       <View>
         <Text className="text-base text-black">{username}</Text>
       </View>
       <View>
         <Text className="text-sm text-gray-500">{score}</Text>
       </View>
-      <TouchableOpacity onPress={handleProfileImagePress}>
+      <TouchableOpacity onPress={() => handleProfileImagePress(user_id)}>
         <Image
-          source={{ uri: username_profile_url }}
+          source={{ uri: profile_image_url }}
           className="w-12 h-12 rounded-2xl"
         />
       </TouchableOpacity>
