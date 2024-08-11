09b74ebdd8076707cb3a7911104f45e475b590ff
Kostek <ksytnik73kwer@gmail.com>
2024-08-11 13:52:42 +0200

    Update TestKnowledgeSection component to accept quizzes as props and add arrays to StaticData file

--- frontend/components/Quizzes/TestKnowledgeSection.tsx
+++ frontend/components/Quizzes/TestKnowledgeSection.tsx
@@ -2,42 +2,11 @@ import { Text, View } from 'react-native';
 import StartQuizList from './StartQuizList';
 import { StartQuizProps } from '@/types/quizzes';
 
-const TestKnowledgeSection = () => {
-  const quizzes: StartQuizProps[] = [
-    {
-      quiz_id: 1,
-      level: 'Beginner',
-      questionsCount: 10,
-      title: 'Ecology basics',
-      description:
-        'Questions about ecosystems, biodiversity, and basic ecological concepts',
-    },
-    {
-      quiz_id: 2,
-      level: 'Intermediate',
-      questionsCount: 15,
-      title: 'Climate Change',
-      description:
-        'Questions about the causes, impacts, and solutions to climate change',
-    },
-    {
-      quiz_id: 3,
-      level: 'Intermediate',
-      questionsCount: 20,
-      title: 'Sustainable Development',
-      description:
-        'Questions about sustainable development goals and strategies',
-    },
-    {
-      quiz_id: 4,
-      level: 'Advanced',
-      questionsCount: 25,
-      title: 'Conservation Biology',
-      description:
-        'Questions about conservation strategies and endangered species',
-    },
-  ];
+interface TestKnowledgeSectionProps {
+  quizzes: StartQuizProps[];
+}
 
+const TestKnowledgeSection = ({ quizzes }: TestKnowledgeSectionProps) => {
   return (
     <View className="p-3">
       <Text className="text-xl mb-4">Test Your Knowledge</Text>
--- frontend/constants/StaticData.ts
+++ frontend/constants/StaticData.ts
@@ -1,3 +1,5 @@
+import { StartQuizProps } from '@/types/quizzes';
+
 const airQualityTips = [
   'Change Filters Regularly',
   'Invest In An Air Purifier',
@@ -6,4 +8,50 @@ const airQualityTips = [
   'Use Natural Cleaning Products',
 ];
 
-export { airQualityTips };
+const quizzes: StartQuizProps[] = [
+  {
+    quiz_id: 1,
+    level: 'Beginner',
+    questionsCount: 10,
+    title: 'Ecology basics',
+    description:
+      'Questions about ecosystems, biodiversity, and basic ecological concepts',
+  },
+  {
+    quiz_id: 2,
+    level: 'Intermediate',
+    questionsCount: 15,
+    title: 'Climate Change',
+    description:
+      'Questions about the causes, impacts, and solutions to climate change',
+  },
+  {
+    quiz_id: 3,
+    level: 'Intermediate',
+    questionsCount: 20,
+    title: 'Sustainable Development',
+    description: 'Questions about sustainable development goals and strategies',
+  },
+  {
+    quiz_id: 4,
+    level: 'Advanced',
+    questionsCount: 25,
+    title: 'Conservation Biology',
+    description:
+      'Questions about conservation strategies and endangered species',
+  },
+];
+
+const quizOfTheDay: StartQuizProps = {
+  quiz_id: 1,
+  level: 'Beginner',
+  questionsCount: 5,
+  title: 'Human Impact on the Environment',
+  description: [
+    'How human activities like deforestation, urbanization, and agriculture affect the environment.',
+    'The concept of ecological footprints and how to reduce them.',
+    'Case studies of significant environmental changes caused by human actions.',
+  ],
+};
+
+export { airQualityTips, quizzes, quizOfTheDay };
