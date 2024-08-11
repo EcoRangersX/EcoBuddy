c9d4dc3555d1ec8054af2024363f998df647fdda
Kostek <ksytnik73kwer@gmail.com>
2024-08-11 10:50:40 +0200

    Update Quizzes Screen with QuizOfTheDay Section

--- frontend/app/(tabs)/quizzes.tsx
+++ frontend/app/(tabs)/quizzes.tsx
@@ -1,12 +1,31 @@
 import { ScrollView, View } from 'react-native';
 import Header from '@/components/Header';
 import TestKnowledgeSection from '@/components/Quizzes/TestKnowledgeSection';
+import QuizOfTheDaySection from '@/components/Quizzes/QuizOfTheDaySection';
+import { StartQuizProps } from '@/types/quizzes';
 
-export default function ArticlesScreen() {
+export default function QuizzesScreen() {
+  const quizOfTheDay: StartQuizProps = {
+    quiz_id: 1,
+    level: 'Beginner',
+    questionsCount: 5,
+    title: 'Ecology Quiz',
+    description: 'Test your knowledge about ecology',
+  };
+
   return (
     <ScrollView>
       <Header />
       <TestKnowledgeSection />
+      <View className="mt-5">
+        <QuizOfTheDaySection
+          quiz_id={quizOfTheDay.quiz_id}
+          level={quizOfTheDay.level}
+          title={quizOfTheDay.title}
+          description={quizOfTheDay.description}
+          questionsCount={quizOfTheDay.questionsCount}
+        />
+      </View>
     </ScrollView>
   );
 }
