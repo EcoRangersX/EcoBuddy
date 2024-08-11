fe0a45373f3040a97b330b9033a19a40022c5156
Kostek <ksytnik73kwer@gmail.com>
2024-08-11 12:47:06 +0200

    Update CurrentQuizStreak component with improved styling and use of StreakIcon

--- frontend/components/Quizzes/CurrentQuizStreak.tsx
+++ frontend/components/Quizzes/CurrentQuizStreak.tsx
@@ -1,4 +1,5 @@
-import { View, Text } from 'react-native-reanimated/lib/typescript/Animated';
+import { View, Text } from 'react-native';
+import { StreakIcon } from '../Icons/QuizzesIcons';
 
 export interface CurrentQuizStreakProps {
   streak?: number;
@@ -6,11 +7,11 @@ export interface CurrentQuizStreakProps {
 
 const CurrentQuizStreak = ({ streak = 0 }: CurrentQuizStreakProps) => {
   return (
-    <View className="bg-white rounded-full justify-between shadow-md shadow-black border-2 border-[#4ac965]">
-      <Text>Streak</Text>
-      <View className="">
-        {/* <StreakIcon /> */}
-        <Text>{streak}</Text>
+    <View className="flex-row items-center justify-between rounded-full p-2 w-1/2 border-2 border-[#4ac965]">
+      <Text className='text-base'>Streak</Text>
+      <View className="bg-white rounded-full items-center justify-center py-2 w-1/2 flex-row">
+        <StreakIcon size={36} />
+        <Text className='text-base'>{streak}</Text>
       </View>
     </View>
   );
