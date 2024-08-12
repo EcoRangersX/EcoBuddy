90d443197f7c587f2fdd9f80b08b3ae9b1dd680b
Kostek <ksytnik73kwer@gmail.com>
2024-08-12 14:39:22 +0200

    Update Articles Screen with SearchBar component

--- frontend/app/(tabs)/articles.tsx
+++ frontend/app/(tabs)/articles.tsx
@@ -1,12 +1,16 @@
-import { ScrollView } from 'react-native';
+import { ScrollView,View } from 'react-native';
 import Header from '@/components/Header';
 import EcoFriendlyArticlesSection from '@/components/Articles/EcoFriendlyArticlesSection';
 import { ecoFriendlyArticles } from '@/constants/StaticData';
+import SearchBar from '@/components/Articles/SearchBar';
 
 export default function ArticlesScreen() {
   return (
     <ScrollView>
       <Header />
+      <View className="px-3 py-1">
+        <SearchBar />
+      </View>
       <EcoFriendlyArticlesSection articles={ecoFriendlyArticles} />
     </ScrollView>
   );
