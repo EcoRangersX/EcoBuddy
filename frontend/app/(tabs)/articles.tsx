b7b29f06671bc7f68bff35c75e9ff9dd97f9a5e5
Kostek <ksytnik73kwer@gmail.com>
2024-08-15 07:25:01 +0200

    Update Articles Screen with SearchModal component

--- frontend/app/(tabs)/articles.tsx
+++ frontend/app/(tabs)/articles.tsx
@@ -2,14 +2,14 @@ import { ScrollView,View } from 'react-native';
 import Header from '@/components/Header';
 import EcoFriendlyArticlesSection from '@/components/Articles/EcoFriendlyArticlesSection';
 import { ecoFriendlyArticles } from '@/constants/StaticData';
-import SearchBar from '@/components/Articles/SearchBar';
+import SearchModal from '@/components/Articles/SearchModal';
 
 export default function ArticlesScreen() {
   return (
     <ScrollView>
       <Header />
       <View className="px-3 py-1">
-        <SearchBar />
+        <SearchModal />
       </View>
       <EcoFriendlyArticlesSection articles={ecoFriendlyArticles} />
     </ScrollView>
