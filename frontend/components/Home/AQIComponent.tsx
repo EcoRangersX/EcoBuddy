b26d23ddd0047522c41e1d8a1bfcf2cb30472771
Kostek <ksytnik73kwer@gmail.com>
2024-08-16 13:46:23 +0200

    Refactor AQIComponent to reflect loading and error handling

--- frontend/components/Home/AQIComponent.tsx
+++ frontend/components/Home/AQIComponent.tsx
@@ -9,10 +9,18 @@ import { useRouter } from 'expo-router';
 interface AQIComponentProps {
   value: number;
   status: string;
-  location: string;
+  city: string;
+  loading: boolean;
+  error: string | null;
 }
 
-const AQIComponent = ({ value, status, location }: AQIComponentProps) => {
+const AQIComponent = ({
+  value,
+  status,
+  city,
+  loading,
+  error,
+}: AQIComponentProps) => {
   const router = useRouter();
 
   const handleViewDetails = () => {
@@ -25,41 +33,51 @@ const AQIComponent = ({ value, status, location }: AQIComponentProps) => {
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 1 }}
       className="rounded-[30px] w-[90%]">
-      <View className="p-5 items-center justify-center">
-        <View className="flex flex-row items-center mb-3">
-          <LocationIcon color="white" />
-          <Text className="text-white text-base ml-1">{location}</Text>
+      {loading ? (
+        <View>
+          <Text>Loading...</Text>
         </View>
-        <View className="items-center justify-center">
-          <Svg width={190} height={190} viewBox="0 0 190 190">
-            <AnimatedCircularProgress
-              size={190}
-              width={8}
-              fill={value}
-              tintColor="#3dbcab"
-              backgroundColor="#fff"
-              lineCap="round">
-              {() => (
-                <View className="flex flex-col">
-                  <Text className="text-white text-4xl self-center">
-                    {value}
-                  </Text>
-                  <Text className="text-white text-lg self-center">
-                    {capitalize(status)}
-                  </Text>
-                </View>
-              )}
-            </AnimatedCircularProgress>
-          </Svg>
+      ) : error ? (
+        <View>
+          {error && <Text>Error during fetching the Location: {error}</Text>}
         </View>
-        <Text className="text-white text-sm mb-4">EU AQI</Text>
-        <TouchableOpacity
-          onPress={handleViewDetails}
-          className="flex flex-row items-center bg-white px-7 py-2 rounded-full">
-          <Text className="text-black text-base mr-2">View Details</Text>
-          <ViewDetailsIcon size={30} />
-        </TouchableOpacity>
-      </View>
+      ) : (
+        <View className="p-5 items-center justify-center">
+          <View className="flex flex-row items-center mb-3">
+            <LocationIcon color="white" />
+            <Text className="text-white text-base ml-1">{city}</Text>
+          </View>
+          <View className="items-center justify-center">
+            <Svg width={190} height={190} viewBox="0 0 190 190">
+              <AnimatedCircularProgress
+                size={190}
+                width={8}
+                fill={value}
+                tintColor="#3dbcab"
+                backgroundColor="#fff"
+                lineCap="round">
+                {() => (
+                  <View className="flex flex-col">
+                    <Text className="text-white text-4xl self-center">
+                      {value}
+                    </Text>
+                    <Text className="text-white text-lg self-center">
+                      {capitalize(status)}
+                    </Text>
+                  </View>
+                )}
+              </AnimatedCircularProgress>
+            </Svg>
+          </View>
+          <Text className="text-white text-sm mb-4">EU AQI</Text>
+          <TouchableOpacity
+            onPress={handleViewDetails}
+            className="flex flex-row items-center bg-white px-7 py-2 rounded-full">
+            <Text className="text-black text-base mr-2">View Details</Text>
+            <ViewDetailsIcon size={30} />
+          </TouchableOpacity>
+        </View>
+      )}
     </LinearGradient>
   );
 };
