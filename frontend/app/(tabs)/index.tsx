f04818ec78a9903b37525d3e830828ecd63b65a6
Kostek <ksytnik73kwer@gmail.com>
2024-08-16 13:46:35 +0200

    Add location permission handling and fetch air quality data

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, View, Text } from 'react-native';
+import { ScrollView, View, Text, Platform } from 'react-native';
 import AQIComponent from '@/components/Home/AQIComponent';
 import Header from '@/components/Header';
 import WeatherDataSlider from '@/components/Home/WeatherDataSlider';
@@ -8,17 +8,82 @@ import {
   aiQuestions,
   quizTitles,
   articleTitles,
-  ecoTips
+  ecoTips,
 } from '@/constants/EducationArrays';
 import EcoTipList from '@/components/Home/EcoTipList';
+import { useAirQuality } from '@/hooks/useAirQuality';
+import { useState, useEffect } from 'react';
+import * as Device from 'expo-device';
+import * as Location from 'expo-location';
 
 export default function HomeScreen() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null,
+  );
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const { getAirQuality, airQualityData, loading, error } = useAirQuality();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        if (Platform.OS === 'android' && !Device.isDevice) {
+          setErrorMsg(
+            'Oops, this will not work on an Android Emulator. Try it on your device!',
+          );
+          return;
+        }
+
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== Location.PermissionStatus.GRANTED) {
+          setErrorMsg('Permission to access location was denied');
+          return;
+        }
+
+        let currentLocation = await Location.getCurrentPositionAsync({});
+        setLocation(currentLocation);
+      } catch (error) {
+        if (error instanceof Error) {
+          setErrorMsg(error.message);
+        } else {
+          setErrorMsg('An unexpected error occurred');
+        }
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    const fetchAirQualityData = async () => {
+      if (location) {
+        try {
+          await getAirQuality({
+            lon: location.coords.longitude,
+            lat: location.coords.latitude,
+          });
+        } catch (error) {
+          if (error instanceof Error) {
+            setErrorMsg(error.message);
+          } else {
+            setErrorMsg('An unexpected error occurred while fetching air quality data');
+          }
+        }
+      }
+    };
+
+    fetchAirQualityData();
+  }, [location]);
+
   return (
     <ScrollView>
       <Header />
       {/* AQI Component */}
       <View className="mt-5 items-center">
-        <AQIComponent value={36} status='good' location='Gniezno, Greater Poland' />
+        <AQIComponent
+          value={36}
+          status={airQualityData?.AQI || "good"}
+          city={airQualityData?.City || "Warsaw"}
+          loading={loading}
+          error={error}
+        />
       </View>
       {/* Weather Data Section */}
       <View className="mt-5 p-5">
