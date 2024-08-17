ade2b9f736d09ca80a2109cf0f2091e4538dce88
Kostek <ksytnik73kwer@gmail.com>
2024-08-17 21:48:52 +0200

    Add JSDoc comments to leaderboard components

--- frontend/components/LeaderboardUserList.tsx
+++ frontend/components/LeaderboardUserList.tsx
@@ -6,6 +6,17 @@ interface LeaderboardUserListProps {
   users: LeaderboardUserItemProps[];
 }
 
+/**
+ * Renders a list of users for the leaderboard.
+ *
+ * @component
+ * @param {Object[]} users - An array of user objects.
+ * @param {number} users[].user_id - The ID of the user.
+ * @param {string} users[].username - The username of the user.
+ * @param {string} users[].profile_image_url - The URL of the user's profile image.
+ * @param {number} users[].score - The score of the user.
+ * @returns {JSX.Element} The rendered leaderboard user list.
+ */
 const LeaderboardUserList = ({ users }: LeaderboardUserListProps) => {
   return (<View className="bg-white rounded-xl p-2">
     {users.map((user, index) => (
