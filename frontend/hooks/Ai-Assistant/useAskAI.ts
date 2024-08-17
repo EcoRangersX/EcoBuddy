6b2e97b101250a3591acf42ebb56e3afa3fe8254
Kostek <ksytnik73kwer@gmail.com>
2024-08-17 14:21:16 +0200

    Refactor useAskAi hook

--- frontend/hooks/Ai-Assistant/useAskAI.ts
+++ frontend/hooks/Ai-Assistant/useAskAI.ts
@@ -1,57 +1,33 @@
-import { useState } from 'react';
 import axios from 'axios';
+import { useState } from 'react';
 import { BASE_API_URL } from '@/constants/Urls';
 
 interface AIResponse {
   response: string;
 }
 
-// Uncomment for Axios Logging purposes
-// axios.interceptors.request.use(
-//   function (config) {
-//     console.log('Request made with ', config);
-//     return config;
-//   },
-//   function (error) {
-//     console.log('Error making request', error);
-//     return Promise.reject(error);
-//   },
-// );
-
-// Uncomment for Axios Logging purposes
-// // Add a response interceptor
-// axios.interceptors.response.use(
-//   function (response) {
-//     console.log('Response received', response);
-//     return response;
-//   },
-//   function (error) {
-//     console.log('Error in response', error);
-//     return Promise.reject(error);
-//   },
-// );
-
 export function useAskAI() {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
+  const [loadingAiOutput, setLoadingAiOutput] = useState<boolean>(false);
+  const [errorAiOutput, setErrorAiOutput] = useState<string | null>(null);
   const [aiResponse, setAIResponse] = useState<AIResponse | null>(null);
 
-  async function askAI(prompt: string) {
-    setLoading(true);
-    setError(null);
+  async function askAI(question: string) {
+    setLoadingAiOutput(true);
+    setErrorAiOutput(null);
     setAIResponse(null);
 
     try {
       const response = await axios.post(`${BASE_API_URL}/api/ai_assistant`, {
-        userPrompt: prompt,
+        "user-input": question,
       });
       setAIResponse(response.data);
     } catch (err: any) {
-      setError(err.message);
+      console.log(`Error during fetching the AI output: ${err.message}`)
+      setErrorAiOutput(err.message);
     } finally {
-      setLoading(false);
+      setLoadingAiOutput(false);
     }
   }
 
-  return { askAI, aiResponse, loading, error };
+  return { askAI, aiResponse, loadingAiOutput, errorAiOutput };
 }
