81e16956694d44a1d9147f6486e05ca1526e3789
Kostek <ksytnik73kwer@gmail.com>
2024-08-18 12:38:03 +0200

    Refactor backend run.py to include host parameter in app.run()

--- backend/run.py
+++ backend/run.py
@@ -5,5 +5,5 @@ from app import create_app,globals_dict
 
 app = create_app()
 if __name__ == '__main__':
-    app.run(debug=globals_dict['debug'],threaded=True)
+    app.run(debug=globals_dict['debug'],threaded=True, host="0.0.0.0")
 
