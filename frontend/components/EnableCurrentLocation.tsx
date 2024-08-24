c7c5cbfd5c9992da3f1da80eca1c33e108b8e06a
Kostek <ksytnik73kwer@gmail.com>
2024-08-24 10:47:25 +0200

    Fix requesting location logic

--- frontend/components/EnableCurrentLocation.tsx
+++ frontend/components/EnableCurrentLocation.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { View, TouchableOpacity, Text, Platform } from 'react-native';
 import * as Device from 'expo-device';
 import * as Location from 'expo-location';
@@ -13,37 +12,35 @@ const EnableCurrentLocation = ({
   setLocationErrorMsg,
 }: EnableCurrentLocationProps) => {
   const requestLocationPermissions = () => {
-    useEffect(() => {
-      (async () => {
-        try {
-          if (Platform.OS === 'android' && !Device.isDevice) {
-            setLocationErrorMsg(
-              'Oops, this will not work on an Android Emulator. Try it on your device!',
-            );
-            return;
-          }
+    if (Platform.OS === 'android' && !Device.isDevice) {
+      setLocationErrorMsg(
+        'Oops, this will not work on an Android Emulator. Try it on your device!',
+      );
+      return;
+    }
 
-          let { status } = await Location.requestForegroundPermissionsAsync();
-          if (status !== Location.PermissionStatus.GRANTED) {
-            setLocationErrorMsg('Permission to access location was denied');
-            return;
-          }
-
-          let currentLocation = await Location.getCurrentPositionAsync({});
-          setLocation(currentLocation);
-        } catch (error) {
-          if (error instanceof Error) {
-            setLocationErrorMsg(error.message);
-          } else {
-            setLocationErrorMsg('An unexpected error occurred');
-          }
+    (async () => {
+      try {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== Location.PermissionStatus.GRANTED) {
+          setLocationErrorMsg('Permission to access location was denied');
+          return;
         }
-      })();
-    }, []);
-  };
+
+        let currentLocation = await Location.getCurrentPositionAsync({});
+        setLocation(currentLocation);
+      } catch (error) {
+        if (error instanceof Error) {
+          setLocationErrorMsg(error.message);
+        } else {
+          setLocationErrorMsg('An unexpected error occurred');
+        }
+      }
+    })();
+  }
 
   return (
-    <View className="p-5 shadow-md shadow-black">
+    <View className="bg-white p-5 shadow-md shadow-black">
       <TouchableOpacity onPress={requestLocationPermissions}>
         <Text className="">Enable Location</Text>
       </TouchableOpacity>
