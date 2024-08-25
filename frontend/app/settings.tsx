a5d0208b2fac2d67f57e92d89c583da7a4394514
Kostek <ksytnik73kwer@gmail.com>
2024-08-25 23:11:49 +0200

    Update Settings page with AccountSettingsSection component

--- frontend/app/settings.tsx
+++ frontend/app/settings.tsx
@@ -1,10 +1,14 @@
-import { ScrollView } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import Header from '@/components/Header';
+import AccountSettingsSection from '@/components/settings/AccountSettingsSection';
 
 export default function Settings() {
   return (
     <ScrollView>
       <Header />
+      <View className='p-5'>
+        <AccountSettingsSection />
+      </View>
     </ScrollView>
   );
 }
