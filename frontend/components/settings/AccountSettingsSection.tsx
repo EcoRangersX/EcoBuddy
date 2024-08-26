36e40ff175fbe0248ff503e4410e6dae04f8b10f
Kostek <ksytnik73kwer@gmail.com>
2024-08-26 10:39:08 +0200

    Add log out and delete account buttons to AccountSettingsSection

--- frontend/components/settings/AccountSettingsSection.tsx
+++ frontend/components/settings/AccountSettingsSection.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import ChooseLanguageDropdown from './ChooseLanguageDropdown';
 import ChooseLocationDropdown from './ChooseLocationDropdown';
 import { countriesStatic } from '@/constants/StaticData';
+import { Button } from 'react-native-paper';
 
 const AccountSettingsSection = () => {
   // TODO: Remove this mock data when we have appropriate hooks implemented
@@ -16,19 +17,25 @@ const AccountSettingsSection = () => {
 
   return (
     <>
-      <View className='mb-4'>
+      <View className="mb-4">
         <ChooseLanguageDropdown
           currentLanguage={language}
           languages={languages}
           onLanguageChange={setLanguage}
         />
       </View>
-      <View className='mb-4'>
+      <View className="mb-4">
         <ChooseLocationDropdown
           location={country}
           locations={countriesStatic}
           onLocationChange={setCountry}
         />
+      </View>
+      <Button mode="outlined">
+        Log out
+      </Button>
+      <View className='mt-4'>
+        <Button mode="outlined">Delete Account</Button>
       </View>
     </>
   );
