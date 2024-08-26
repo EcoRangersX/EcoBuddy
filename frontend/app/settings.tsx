afeed522e885ab84c6d07f1c2c98b65bac35dddd
Kostek Sytnyk <ksytnik73kwer@gmail.com>
2024-08-26 11:16:41 +0200

    Implement Notification Preferences component and enhance Settings Screen (#61)
    * Add Notification Toggle component
    
    * Add NotificationPreferencesSection component
    
    * Update Settings Screen with NotificationPreferencesSection component
    
    * Add Text element to AccountSettingsSection and make small styling changes

--- frontend/app/settings.tsx
+++ frontend/app/settings.tsx
@@ -1,14 +1,14 @@
 import { ScrollView, View } from 'react-native';
 import Header from '@/components/Header';
 import AccountSettingsSection from '@/components/settings/AccountSettingsSection';
+import NotificationPreferencesSection from '@/components/settings/NotificationPreferencesSection';
 
 export default function Settings() {
   return (
     <ScrollView>
       <Header />
-      <View className='p-5'>
-        <AccountSettingsSection />
-      </View>
+      <AccountSettingsSection />
+      <NotificationPreferencesSection />
     </ScrollView>
   );
 }
---
+++ frontend/components/settings/NotificationPreferencesSection.tsx
@@ -0,0 +1,38 @@
+import { View, Text } from 'react-native';
+import { useState } from 'react';
+import NotificationToggle from './NotificationToggle';
+
+interface NotificationPreferencesSectionProps {
+  aqiAlerts?: boolean;
+  quizReminders?: boolean;
+  dailyEcoTip?: boolean;
+  newArticleAlerts?: boolean;
+}
+
+const NotificationPreferencesSection = ({
+  aqiAlerts = true,
+  quizReminders = true,
+  dailyEcoTip = true,
+  newArticleAlerts = true,
+}: NotificationPreferencesSectionProps) => {
+  const [aqiAlertsEnabled, setAqiAlertsEnabled] = useState(aqiAlerts);
+  const [quizRemindersEnabled, setQuizRemindersEnabled] =
+    useState(quizReminders);
+  const [dailyEcoTipEnabled, setDailyEcoTipEnabled] = useState(dailyEcoTip);
+  const [newArticleAlertsEnabled, setNewArticleAlertsEnabled] =
+    useState(newArticleAlerts);
+
+  return (
+    <View className="p-4">
+      <Text className="text-lg rounded-full mb-4 text-center p-2 bg-white border-2 border-[#000000]">
+        Notification Preferences
+      </Text>
+      <NotificationToggle label='Aqi Alerts' value={aqiAlertsEnabled} onValueChange={setAqiAlertsEnabled} />
+      <NotificationToggle label='Quiz Reminders' value={quizRemindersEnabled} onValueChange={setQuizRemindersEnabled} />
+      <NotificationToggle label='Daily Eco Tip' value={dailyEcoTipEnabled} onValueChange={setDailyEcoTipEnabled} />
+      <NotificationToggle label='New Article Alerts' value={newArticleAlertsEnabled} onValueChange={setNewArticleAlertsEnabled} />
+    </View>
+  );
+};
+
+export default NotificationPreferencesSection;
