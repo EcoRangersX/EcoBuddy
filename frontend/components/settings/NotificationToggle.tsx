6152102d2534354d95fe170496c98ada96593d68
Kostek <ksytnik73kwer@gmail.com>
2024-08-26 11:10:55 +0200

    Add NotificationPreferencesSection component

--- frontend/components/settings/NotificationToggle.tsx
+++ frontend/components/settings/NotificationToggle.tsx
@@ -9,7 +9,7 @@ interface NotificationToggleProps {
 
 const NotificationToggle = ({ value, onValueChange, label }: NotificationToggleProps) => {
   return (
-    <View className="bg-white rounded-full p-1 w-2/3 flex-row shadow-sm shadow-black justify-evenly items-center">
+    <View className="bg-white rounded-full p-1 w-9/12 mb-3 flex-row shadow-sm shadow-black justify-evenly items-center">
       <Text className="text-lg">{label}</Text>
       <Switch value={value} onValueChange={onValueChange} color="#2abb64" />
     </View>
