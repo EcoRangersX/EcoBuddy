5de7ef38b855d6d3506482399e7cfdc7dc30d902
Kostek <ksytnik73kwer@gmail.com>
2024-09-01 13:12:29 +0200

    Add QuizIntroduction component and improve QuizScreen layout

--- frontend/app/(tabs)/quizzes/[id].tsx
+++ frontend/app/(tabs)/quizzes/[id].tsx
@@ -1,12 +1,17 @@
 import { View, Text } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
+import QuizIntroduction from '@/components/quiz/QuizIntroduction';
+import { quizStepsStatic } from '@/constants/StaticData';
 
 export default function QuizScreen() {
   const { id } = useLocalSearchParams();
 
   return (
     <View className="">
-      <Text className="">Quiz Screen with the id: {id}</Text>
+      <Text className="text-xl font-semibold">Quiz Screen with the id: {id}</Text>
+      <View className=''>
+        <QuizIntroduction steps={quizStepsStatic} />
+      </View>
     </View>
   );
 }
