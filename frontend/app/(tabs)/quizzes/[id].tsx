ce76ced219371c3316047ff9f2463bb827447cb8
Kostek <ksytnik73kwer@gmail.com>
2024-09-01 13:39:54 +0200

    Update quizzes/[id] Screen with hooks to manage QuizIntroduction component

--- frontend/app/(tabs)/quizzes/[id].tsx
+++ frontend/app/(tabs)/quizzes/[id].tsx
@@ -1,17 +1,26 @@
 import { View, Text } from 'react-native';
+import { useState } from 'react';
 import { useLocalSearchParams } from 'expo-router';
 import QuizIntroduction from '@/components/quiz/QuizIntroduction';
 import { quizStepsStatic } from '@/constants/StaticData';
 
 export default function QuizScreen() {
+  const [isVisible, setIsVisible] = useState(true);
   const { id } = useLocalSearchParams();
+
+  const handleOnClose = () => {
+    setIsVisible(false);
+  };
+
+  // TODO: Implement hook to fetch quiz data using id from local search params
 
   return (
     <View className="">
-      <Text className="text-xl font-semibold">Quiz Screen with the id: {id}</Text>
-      <View className=''>
-        <QuizIntroduction steps={quizStepsStatic} />
-      </View>
+      <QuizIntroduction
+        steps={quizStepsStatic}
+        isVisible={isVisible}
+        onClose={handleOnClose}
+      />
     </View>
   );
 }
