55b696edd0111a5e94ee7616d6aff9019637f030
Kostek <ksytnik73kwer@gmail.com>
2024-09-08 22:52:25 +0200

    Add useQuizzes hook to QuizzesScreen and use it inside useEffect hook

--- frontend/app/(tabs)/quizzes/index.tsx
+++ frontend/app/(tabs)/quizzes/index.tsx
@@ -1,14 +1,22 @@
 import { ScrollView, View } from 'react-native';
+import { useEffect } from 'react';
 import Header from '@/components/Header';
 import TestKnowledgeSection from '@/components/quizzes/TestKnowledgeSection';
 import QuizOfTheDaySection from '@/components/quizzes/QuizOfTheDaySection';
-import { quizzesStatic, quizOfTheDayStatic } from '@/constants/StaticData';
+import { quizOfTheDayStatic } from '@/constants/StaticData';
+import { useQuizzes } from '@/hooks/quizzes/useQuizzes';
 
 export default function QuizzesScreen() {
+  const { getQuizzes, quizzes, loadingQuizzes, quizzesErrorMsg } = useQuizzes();
+
+  useEffect(() => {
+    getQuizzes(5);
+  }, []);  // Fetch quizzes to display as a list
+
   return (
     <ScrollView>
       <Header />
-      <TestKnowledgeSection quizzes={quizzesStatic} />
+      <TestKnowledgeSection quizzes={quizzes?.['quiz-preview']} loading={loadingQuizzes} error={quizzesErrorMsg} />
       <View className="mt-5">
         <QuizOfTheDaySection
           id={quizOfTheDayStatic.id}
