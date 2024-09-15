0da528d046e5a3aa9acfef0c80582fa3e763fcb5
Kostek <ksytnik73kwer@gmail.com>
2024-09-15 20:16:02 +0200

    Refactor quizzes index page to use TopQuizzesNavigationBar component; remove old code

--- frontend/app/(tabs)/quizzes/index.tsx
+++ frontend/app/(tabs)/quizzes/index.tsx
@@ -1,56 +1,12 @@
-import { ScrollView, View, Text } from 'react-native';
-import { useEffect } from 'react';
+import { View } from 'react-native';
 import Header from '@/components/Header';
-import TestKnowledgeSection from '@/components/quizzes/TestKnowledgeSection';
-import QuizOfTheDaySection from '@/components/quizzes/QuizOfTheDaySection';
-import { useQuizzes } from '@/hooks/quizzes/useQuizzes';
-import { useFeaturedQuiz } from '@/hooks/quizzes/useFeaturedQuiz';
+import TopQuizzesNavigationBar from '@/components/quizzes/TopQuizzesNavigationBar';
 
 export default function QuizzesScreen() {
-  const { getQuizzes, quizzes, loadingQuizzes, quizzesErrorMsg } = useQuizzes();
-  const {
-    getFeaturedQuiz,
-    featuredQuiz,
-    loadingFeaturedQuiz,
-    featuredFeaturedErrorMsg,
-  } = useFeaturedQuiz();
-
-  useEffect(() => {
-    getQuizzes(5);
-    getFeaturedQuiz();
-  }, []); // Fetch quizzes and featured quiz data
-
   return (
-    <ScrollView>
+    <View className='flex-1'>
       <Header />
-      <TestKnowledgeSection
-        quizzes={quizzes?.['quiz-preview']}
-        loading={loadingQuizzes}
-        error={quizzesErrorMsg}
-      />
-      <View className="mt-5">
-        {loadingFeaturedQuiz ? (
-          <View>
-            <Text>Loading...</Text>
-          </View>
-        ) : featuredFeaturedErrorMsg ? (
-          <View>
-            <Text>{featuredFeaturedErrorMsg} Contact Support Team</Text>
-          </View>
-        ) : featuredQuiz ? (
-          <QuizOfTheDaySection
-            id={featuredQuiz.id}
-            level={featuredQuiz.level}
-            title={featuredQuiz.title}
-            description={featuredQuiz.description}
-            amount-of-questions={featuredQuiz['amount-of-questions']}
-          />
-        ) : (
-          <View>
-            <Text>No featured quiz available</Text>
-          </View>
-        )}
-      </View>
-    </ScrollView>
+      <TopQuizzesNavigationBar />
+    </View>
   );
 }
