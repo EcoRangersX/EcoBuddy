4033f1ac8e10abf6d47c74e80166e0aa19511c17
Kostek <ksytnik73kwer@gmail.com>
2024-09-15 19:39:41 +0200

    Add Badge component and update BadgesScreen

--- frontend/app/(tabs)/badges.tsx
+++ frontend/app/(tabs)/badges.tsx
@@ -1,10 +1,11 @@
-import { ScrollView, Text } from "react-native";
-
+import { ScrollView, Text } from 'react-native';
+import Badge from '@/components/badges/Badge';
 
 export default function BadgesScreen() {
   return (
     <ScrollView className="p-10">
-        <Text className="text-2xl">Badges Page</Text>
+      <Text className="text-2xl">Badges Page</Text>
+      <Badge text="Beginner" />
     </ScrollView>
   );
 }
