abf7432d8947056ccfddf94a55b62201f423adc5
Kostek <ksytnik73kwer@gmail.com>
2024-09-15 20:03:57 +0200

    Update TopTabs to include QuizTab and rename LeaderboardScreen to LeaderboardTab

--- frontend/components/quiz/TopTabs.tsx
+++ frontend/components/quiz/TopTabs.tsx
@@ -1,13 +1,15 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import LeaderboardScreen from '@/components/leaderboard/LeaderboardTab';
+import LeaderboardTab from '@/components/leaderboard/LeaderboardTab';
 import BadgesScreen from '@/components/badges/BadgesTab';
+import QuizTab from './QuizTab';
 
 const TopTab = createMaterialTopTabNavigator();
 
 export default function TopTabs() {
   return (
-    <TopTab.Navigator initialRouteName='Leaderboard'>
-      <TopTab.Screen name="Leaderboard" component={LeaderboardScreen} />
+    <TopTab.Navigator initialRouteName='Quiz'>
+      <TopTab.Screen name="Quiz" component={QuizTab} />
+      <TopTab.Screen name="Leaderboard" component={LeaderboardTab} />
       <TopTab.Screen name="Badges" component={BadgesScreen} />
     </TopTab.Navigator>
   );
