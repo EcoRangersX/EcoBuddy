9cda4b813d1cedcb89d20fef4b0f14e68cb763e4
Kostek <ksytnik73kwer@gmail.com>
2024-09-15 20:15:26 +0200

    Rename TopTabs to TopQuizzesNavigationBar and move to quizzes folder

--- frontend/components/quizzes/TopQuizzesNavigationBar.tsx
+++ frontend/components/quizzes/TopQuizzesNavigationBar.tsx
@@ -1,11 +1,11 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import LeaderboardTab from '@/components/leaderboard/LeaderboardTab';
 import BadgesScreen from '@/components/badges/BadgesTab';
-import QuizTab from './QuizTab';
+import QuizTab from '../quiz/QuizTab';
 
 const TopTab = createMaterialTopTabNavigator();
 
-export default function TopTabs() {
+export default function TopQuizzesNavigationBar() {
   return (
     <TopTab.Navigator initialRouteName='Quiz'>
       <TopTab.Screen name="Quiz" component={QuizTab} />
