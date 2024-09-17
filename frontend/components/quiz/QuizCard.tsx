7c6fb73f18e940590ac687e40f2b2137155bee8e
Kostek <ksytnik73kwer@gmail.com>
2024-09-17 18:36:16 +0200

    Refactor QuizCard component to improve question navigation and state management

--- frontend/components/quiz/QuizCard.tsx
+++ frontend/components/quiz/QuizCard.tsx
@@ -1,56 +1,38 @@
 import { useState } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
 import AnswerOption from './AnswerOption';
 import QuizHeader from './QuizHeader';
+import QuizNavigationBottom from './QuizNavigationBottom';
+
+interface QuestionProps {
+  id: number;
+  question: string;
+  options: { id: number; text: string };
+  'correct-answer': number;
+}
 
 interface QuizScreenProps {
   quiz_id: number;
-  questionNumber: number;
+  questions: QuestionProps[];
   totalQuestions: number;
-  question: string;
-  options: { id: number; text: string }[];
-  correctAnswer: number;
+}
+
+interface AnswerProps {
+  question_id: number;
+  selected_option: number;
 }
 
 const QuestionCard = ({ question }: { question: string }) => (
-  <View className="bg-blue-500 rounded-lg p-6 m-4">
+  <View className="bg-blue-500 rounded-lg p-2 m-4">
     <Text className="text-white text-xl text-center">{question}</Text>
   </View>
 );
 
-const NavigationButtons = ({
-  onSkip,
-  onNext,
-  onSubmit,
-}: {
-  onSkip: () => void;
-  onNext: () => void;
-  onSubmit: () => void;
-}) => {
-  return (
-    <View className="flex-row justify-between p-4">
-      <TouchableOpacity
-        className="bg-gray-300 rounded-full px-6 py-3"
-        onPress={onSkip}>
-        <Text className="text-gray-700 font-semibold">Skip</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        className="bg-blue-500 rounded-full px-6 py-3"
-        onPress={onNext}>
-        <Text className="text-white font-semibold">Next</Text>
-      </TouchableOpacity>
-    </View>
+const QuizCard = ({ quiz_id, totalQuestions, questions }: QuizScreenProps) => {
+  const [selectedAnswers, setSelectedAnswers] = useState<AnswerProps[] | null>(
+    null,
   );
-};
-
-const QuizCard = ({
-  quiz_id,
-  questionNumber,
-  totalQuestions,
-  question,
-  options,
-}: QuizScreenProps) => {
-  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
 
   const handleBookmarkQuiz = () => {
     // TODO: Implement hook to bookmark quiz by quiz_id
@@ -58,18 +40,19 @@ const QuizCard = ({
   };
 
   const previousQuestion = () => {
-    // Implement logic to navigate to the previous question
-    console.log('Navigating to the previous question');
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
   };
 
   const nextQuestion = () => {
-    // Implement logic to navigate to the next question
-    console.log('Navigating to the next question');
+    if (currentIndex < totalQuestions - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
   };
 
   const handleOnSkip = () => {
-    // Implement logic to skip the current question
-    console.log('Skipping the current question');
+    nextQuestion();
   };
 
   const handleOnSubmit = () => {
@@ -77,38 +60,65 @@ const QuizCard = ({
     console.log('Submitting the answers');
   };
 
-  console.log(`Selected answer: ${selectedAnswer}`);
+  const currentQuestion = questions[currentIndex];
+
+  console.log(`Current Question: ${currentQuestion.id}`);
+  console.log(`The selected answers: ${JSON.stringify(selectedAnswers)}`);
 
   return (
-    <View className="flex-1 bg-cyan-300 ">
-      <View className='m-4'>
+    <ScrollView className="bg-cyan-300 p-3">
+      <View className="mb-3">
         <QuizHeader
-          questionNumber={questionNumber}
+          questionNumber={currentIndex + 1}
           totalQuestions={totalQuestions}
           onBack={previousQuestion}
           onBookmark={handleBookmarkQuiz}
         />
       </View>
-      <View className="bg-[#e1f3f7] flex-1">
-        <QuestionCard question={question} />
-        <View className="flex-1 justify-center">
-          {options.map((option, index) => (
-            <AnswerOption
-              key={index}
-              label={index + 1}
-              option={option.text}
-              isSelected={selectedAnswer === option.id}
-              onSelect={() => setSelectedAnswer(option.id)}
-            />
-          ))}
-        </View>
-        <NavigationButtons
+      <View className="bg-[#e1f3f7] rounded-md mb-8">
+        <QuestionCard question={currentQuestion.question} />
+        {currentQuestion.options.map(option => (
+          <AnswerOption
+            key={option.id}
+            label={option.id + 1}
+            option={option.text}
+            isSelected={
+              selectedAnswers?.some(
+                answer =>
+                  answer.question_id === currentQuestion.id &&
+                  answer.selected_option === option.id,
+              ) ?? false
+            }
+            onSelect={() => {
+              const updatedAnswers = selectedAnswers
+                ? [
+                    ...selectedAnswers.filter(
+                      answer => answer.question_id !== currentQuestion.id,
+                    ),
+                    {
+                      question_id: currentQuestion.id,
+                      selected_option: option.id,
+                    },
+                  ]
+                : [
+                    {
+                      question_id: currentQuestion.id,
+                      selected_option: option.id,
+                    },
+                  ];
+              setSelectedAnswers(updatedAnswers);
+            }}
+          />
+        ))}
+        <QuizNavigationBottom
           onSkip={handleOnSkip}
           onNext={nextQuestion}
           onSubmit={handleOnSubmit}
+          totalQuestions={totalQuestions}
+          currentIndex={currentIndex}
         />
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
