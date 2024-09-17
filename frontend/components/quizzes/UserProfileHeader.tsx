34b271950d76bedb7d2a1432429ca360f42b5a04
Kostek <ksytnik73kwer@gmail.com>
2024-09-17 11:52:45 +0200

    Refactor UserProfileHeader component: use capitalizeWord function for level display and improve layout for username

--- frontend/components/quizzes/UserProfileHeader.tsx
+++ frontend/components/quizzes/UserProfileHeader.tsx
@@ -1,5 +1,6 @@
 import { View, Text, Image } from 'react-native';
 import { StarIcon } from '../icons/LeaderboardIcons';
+import capitalizeWord from '@/utils/capitalizeWord';
 
 interface UserProfileHeaderProps {
   profile_image_url: string;
@@ -15,21 +16,29 @@ const UserProfileHeader = ({
   stars,
 }: UserProfileHeaderProps) => {
   return (
-    <View className="flex flex-row px-5 mt-5 items-center justify-between">
+    <View className="flex flex-row p-3 items-center justify-between">
       <View className="flex-row space-x-5">
         <Image
           className="w-20 h-20 rounded-full"
           source={{ uri: profile_image_url }}
         />
-        <View className="items-center space-y-1">
-          <View className='w-max-[50px] whitespace-normal'>
-            <Text className="text-base">{username}</Text>
-          </View>
+        <View className="items-start space-y-1">
+          <Text
+            className="text-base"
+            numberOfLines={1}
+            ellipsizeMode="tail"
+            style={{ maxWidth: 130, overflow: 'hidden' }}>
+            {username}
+          </Text>
           <Text className="bg-white rounded-[30px] p-2 shadow-sm shadow-black">
-            {level}
+            {capitalizeWord(level)}
           </Text>
         </View>
       </View>
+      {/*
+      TODO: Not sure how to place the number of stars element horizontally 
+      since username might be long and it will cause UI issues 
+      */}
       <View className="bg-white rounded-[30px] flex-row p-2 space-x-4 items-center shadow-md shadow-black">
         <View className="w-12 h-12 rounded-full justify-center items-center bg-green-500">
           <StarIcon color="yellow" size={36} />
