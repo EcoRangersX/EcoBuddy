ff76ecc626eeab2bc9648e25133ed02035839893
Kostek <ksytnik73kwer@gmail.com>
2024-09-24 21:48:40 +0200

    Add SignUp and LogIn buttons to WelcomeScreen for user authentication

--- frontend/app/(tabs)/welcome.tsx
+++ frontend/app/(tabs)/welcome.tsx
@@ -1,18 +1,42 @@
 import { View, Text, Image } from 'react-native';
 import AppLogo from '@/components/icons/AppLogo';
+import { Button } from 'react-native-paper';
 
 export default function WelcomeScreen() {
+  const handleCreateAccount = () => {};
+
+  const handleLogin = () => {};
+
   return (
-    <View className='flex-1 relative'>
+    <View className="flex-1 relative">
       <Image
-        className='absolute inset-0 w-full h-full object-cover'
+        className="absolute inset-0 w-full h-full object-cover"
         source={require('@/assets/images/bg-welcome-screen.png')}
       />
-      <View className='flex p-8 mt-20'>
+      <View className="flex p-8 mt-12">
         <AppLogo width={130} height={130} />
-        <Text className="text-lg text-white font-semibold">
+        <Text className="text-xl text-white font-semibold">
           Your Companion for a Greener Tomorrow
         </Text>
+      </View>
+      {/* SignUp/LogIn buttons section */}
+      <View className="flex-1 justify-end mb-5 space-y-4 p-4">
+        <Button
+          onPress={handleCreateAccount}
+          mode="contained"
+          className="bg-[#098323] rounded-[40px]">
+          <Text className="text-white text-center text-lg">
+            Create an account
+          </Text>
+        </Button>
+        <Button
+          onPress={handleLogin}
+          mode="contained"
+          className="bg-[#098323] rounded-[40px]">
+          <Text className="text-white text-center text-lg">
+            Log in with EcoBuddy
+          </Text>
+        </Button>
       </View>
     </View>
   );
