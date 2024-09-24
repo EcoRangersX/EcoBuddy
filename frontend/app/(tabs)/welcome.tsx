fbb67fe1d202c3d2b0813799013776d3c3727dd6
Kostek <ksytnik73kwer@gmail.com>
2024-09-24 19:19:11 +0200

    Enhance WelcomeScreen with background image and logo

--- frontend/app/(tabs)/welcome.tsx
+++ frontend/app/(tabs)/welcome.tsx
@@ -1,9 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from 'react-native';
+import AppLogo from '@/components/icons/AppLogo';
 
 export default function WelcomeScreen() {
   return (
-    <View>
-      <Text>Welcome to Expo!</Text>
+    <View className='flex-1 relative'>
+      <Image
+        className='absolute inset-0 w-full h-full object-cover'
+        source={require('@/assets/images/bg-welcome-screen.png')}
+      />
+      <View className='flex p-8 mt-20'>
+        <AppLogo width={130} height={130} />
+        <Text className="text-lg text-white font-semibold">
+          Your Companion for a Greener Tomorrow
+        </Text>
+      </View>
     </View>
   );
 }
