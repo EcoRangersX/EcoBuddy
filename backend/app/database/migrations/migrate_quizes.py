e76e686183d6d0cf0cb3cdfe53de09c08f1d5cf8
Mateusz Kowalski <towermanxxl@gmail.com>
2024-10-13 19:58:17 +0200

    changes response format in quiz endpoint

--- backend/app/database/migrations/migrate_quizes.py
+++ backend/app/database/migrations/migrate_quizes.py
@@ -10,58 +10,58 @@ class Migrate_quizes():
         conn.commit()
 
         q = """{
-    "title": "Ecology Fundamentals Quiz",
-    "level": "Intermediate",
-    "description": "This quiz tests your knowledge of basic ecological concepts, including ecosystems, biodiversity, and conservation.",
-    "quiz": [
-        {
-            "question": "What is the primary source of energy for most ecosystems?",
-            "answers": [
-                "The Sun",
-                "Geothermal energy",
-                "Chemical energy"
-            ],
-            "correct-answer": 0
-        },
-        {
-            "question": "Which of the following is a biotic component of an ecosystem?",
-            "answers": [
-                "Sunlight",
-                "Water",
-                "Plants"
-            ],
-            "correct-answer": 2
-        },
-        {
-            "question": "What term describes the variety of life found within an ecosystem?",
-            "answers": [
-                "Biodiversity",
-                "Productivity",
-                "Biomass"
-            ],
-            "correct-answer": 0
-        },
-        {
-            "question": "What is the main consequence of habitat destruction on biodiversity?",
-            "answers": [
-                "Increased species migration",
-                "Loss of species",
-                "Increased ecosystem stability"
-            ],
-            "correct-answer": 1
-        },
-        {
-            "question": "Which of the following is an example of an abiotic factor in an ecosystem?",
-            "answers": [
-                "Temperature",
-                "Bacteria",
-                "Animals"
-            ],
-            "correct-answer": 0
+            "title": "Ecology Fundamentals Quiz",
+            "level": "Intermediate",
+            "description": "This quiz tests your knowledge of basic ecological concepts, including ecosystems, biodiversity, and conservation.",
+            "quiz": [
+                {
+                    "question": "What is the primary source of energy for most ecosystems?",
+                    "options": [
+                        { "id": 0, "text": "The Sun" },
+                        { "id": 1, "text": "Geothermal energy" },
+                        { "id": 2, "text": "Chemical energy" }
+                    ],
+                    "correct-answer": 0
+                },
+                {
+                    "question": "Which of the following is a biotic component of an ecosystem?",
+                    "options": [
+                        { "id": 0, "text": "Sunlight" },
+                        { "id": 1, "text": "Water" },
+                        { "id": 2, "text": "Plants" }
+                    ],
+                    "correct-answer": 2
+                },
+                {
+                    "question": "What term describes the variety of life found within an ecosystem?",
+                    "options": [
+                        { "id": 0, "text": "Biodiversity" },
+                        { "id": 1, "text": "Productivity" },
+                        { "id": 2, "text": "Biomass" }
+                    ],
+                    "correct-answer": 0
+                },
+                {
+                    "question": "What is the main consequence of habitat destruction on biodiversity?",
+                    "options": [
+                        { "id": 0, "text": "Increased species migration" },
+                        { "id": 1, "text": "Loss of species" },
+                        { "id": 2, "text": "Increased ecosystem stability" }
+                    ],
+                    "correct-answer": 1
+                },
+                {
+                    "question": "Which of the following is an example of an abiotic factor in an ecosystem?",
+                    "options": [
+                        { "id": 0, "text": "Temperature" },
+                        { "id": 1, "text": "Bacteria" },
+                        { "id": 2, "text": "Animals" }
+                    ],
+                    "correct-answer": 0
+                }
+            ]
         }
-    ]
-}
-"""
+        """
         
         
         cursor.execute(
--- backend/run.py
+++ backend/run.py
@@ -4,6 +4,7 @@ from app import create_app,globals_dict
 
 
 app = create_app()
-if __name__ == '__main__':
-    app.run(debug=globals_dict['debug'],threaded=True, host="0.0.0.0")
+
+
+app.run(debug=globals_dict['debug'],threaded=True, host="0.0.0.0")
 
